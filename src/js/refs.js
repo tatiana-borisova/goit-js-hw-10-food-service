98ebe54a1fff896942d9ec893517359722c7f49b
Tatiana Borisova <81636916+tatiana-borisova@users.noreply.github.com>
2021-08-31 13:50:50 +0200

    Start

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default {
+  body: document.querySelector('body'),
+  formEl: document.getElementById('form'),
+  ulEl: document.querySelector('.list'),
+  inputEl: document.querySelector('[name="search"]'),
+  btnEl: document.querySelector('[type="button"]'),
+};
